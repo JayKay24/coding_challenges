c785adf1afb8f52264a5ead28ece6321b6616510
James Kinyua <jameskinyua590@gmail.com>
2020-06-04 23:44:00 +0300

    solve minSubArraySum of educative

--- educative/smallest_subarray_sum.js
+++ educative/smallest_subarray_sum.js
@@ -1,35 +1,19 @@
-function smallestSubArray(array, num) {
-  let winSum = 0;
-  let winStart = 0;
-  let minAbs = Infinity;
-  let smallestArr = array.length;
-  let currentArr = [];
+const findSmallestSubArray = (array, S) => {
+  let windowSum = 0, windowStart = 0, minLength = Infinity;
 
-  for (let winEnd = 0; winEnd < array.length; winEnd++) {
-    currentArr = array.slice(winStart, winEnd + 1);
-    winSum += array[winEnd];
-    if (winSum === num) {
-      smallestArr = array.slice(winStart, winEnd + 1);
-      break;
-    }
+  for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
+    windowSum += array[windowEnd];
 
-    if (winSum > num) {
-      // let currentAbs = Math.abs(num - winSum);
-      if (currentArr.length < smallestArr) {
-        smallestArr = array.slice(winStart, winEnd + 1).length;
-        winSum -= array[winStart];
-        winStart++;
-      }
-      // if (currentAbs < minAbs) {
-      //   smallestArr = array.slice(winStart, winEnd + 1);
-      //   minAbs = currentAbs;
-      //   winSum -= array[winStart];
-      // }
-      // winSum -= array[winStart];
-      // winStart++;
+    while (windowSum >= S) {
+      minLength = Math.min(windowEnd - windowStart + 1, minLength);
+      windowSum -= array[windowStart];
+      windowStart++;
     }
   }
-  return smallestArr;
-}
 
-console.log(smallestSubArray([2, 1, 5, 2, 3, 2], 7));
+  return minLength === Infinity ? 0 : minLength;
+};
+
+console.log(findSmallestSubArray([2, 1, 5, 2, 8], 7));
+console.log(findSmallestSubArray([2, 1, 5, 2, 3, 2], 7));
+console.log(findSmallestSubArray([3, 4, 1, 1, 6], 8));
