1d63d571513042bc903ec20e4c89f10d8c2d4d81
James Kinyua <jameskinyua590@gmail.com>
2020-07-29 10:29:07 +0300

    solve validate subsequence problem from algoexpert

--- algo_expert/valid_subsequence.js
+++ algo_expert/valid_subsequence.js
@@ -1,9 +1,18 @@
-const isValidSubsequence = (array, sequence) => {
-  let winStart = 0, result = false;
+const validateSubsequence = (array, sequence) => {
+  let seqPointer = 0;
 
-  for (let winEnd = 0; winEnd < array.length; i++) {
-    
+  for (let num of array) {
+    if (sequence[seqPointer] === num) {
+      seqPointer++;
+
+      if (sequence.length === seqPointer)
+        return true;
+    }
   }
+
+  return false;
 };
 
-console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]));
+let result = validateSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]);
+// let result = validateSubsequence([1, 2, 3, 4], [9, 3, 4]);
+console.log(result);
