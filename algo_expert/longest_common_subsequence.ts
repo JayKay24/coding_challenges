632ff5c3d2185807b03a60bf1a764c96c2c00a12
James Njuguna <jameskinyua590@gmail.com>
2020-11-24 17:24:02 +0300

    solve longest common subsequence problem in algoexpert

--- algo_expert/longest_common_subsequence.ts
+++ algo_expert/longest_common_subsequence.ts
@@ -1,44 +1,29 @@
 export function longestCommonSubsequence(str1: string, str2: string) {
-  const result: string[] = [],
-    dp = new Array(str2.length + 1).fill(new Array(str1.length + 1).fill([]));
+  const dp: number[][] = new Array(str2.length + 1).fill(
+    new Array(str1.length + 1).fill(0)
+  );
 
-  for (let i = 1; i < str2.length + 1; i++) {
-    for (let j = 1; j < str1.length + 1; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = [...dp[i - 1][j - 1], ...str2[i - 1]];
+  const idxs = new Array(str2.length + 1).fill(
+    new Array(str1.length + 1).fill("")
+  );
+
+  for (let i = 1; i < idxs.length; i++) {
+    for (let j = 1; j < idxs[i].length; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        idxs[i][j] = idxs[i - 1][j - 1] + str2[i - 1];
       } else {
-        dp[i][j] =
-          dp[i - 1][j].length > dp[i][j - 1].length
-            ? dp[i - i][j]
-            : dp[i][j - 1];
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        idxs[i][j] =
+          idxs[i][j - 1].length > idxs[i - 1][j].length
+            ? idxs[i][j - 1]
+            : idxs[i - 1][j];
       }
     }
   }
 
-  return dp[str2.length][str1.length];
+  return idxs[str2.length][str1.length].split("");
 }
 
-// export function longestCommonSubsequence(str1: string, str2: string) {
-//   const lcs: string[][][] = [];
-//   for (let i = 0; i < str2.length + 1; i++) {
-//     const row = new Array(str1.length + 1).fill([]);
-//     lcs.push(row);
-//   }
-//   for (let i = 1; i < str2.length + 1; i++) {
-//     for (let j = 1; j < str1.length + 1; j++) {
-//       if (str2[i - 1] === str1[j - 1]) {
-//         lcs[i][j] = lcs[i - 1][j - 1].concat(str2[i - 1]);
-//       } else {
-//         lcs[i][j] =
-//           lcs[i - 1][j].length > lcs[i][j - 1].length
-//             ? lcs[i - 1][j]
-//             : lcs[i][j - 1];
-//       }
-//     }
-//   }
-
-//   return lcs[str2.length][str1.length];
-// }
-
-let res = longestCommonSubsequence("bd", "abcd");
+let res = longestCommonSubsequence("ZXVVYZW", "XKYKZPW");
 console.log(res);
